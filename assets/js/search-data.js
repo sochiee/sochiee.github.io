3b0eadbf619eebc785669edbcd98b9e64b8194ba
sochiee <softsailors84@ciencias.unam.mx>
2026-06-03 07:09:33 +0000

    Deploying to gh-pages from @ sochiee/sochiee.github.io@3d1b6413e06c0d58893b5bd6ca066d4f5b1eca3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,34 +406,34 @@ ninja.data = [{
             },},{id: "news-um-anúncio-simples-em-uma-linha-com-markdown-emoji-sparkles-smile",
           title: 'Um anúncio simples em uma linha com Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "",},{id: "projects-projeto-1",
-          title: 'projeto 1',
-          description: "com imagem de fundo",
+          section: "",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
           section: "",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-projeto-2",
-          title: 'projeto 2',
-          description: "um projeto com imagem de fundo e comentários do giscus",
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
           section: "",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-projeto-3-com-um-nome-bem-longo",
-          title: 'projeto 3 com um nome bem longo',
-          description: "um projeto que redireciona pra outro website",
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
           section: "",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-projeto-4",
-          title: 'projeto 4',
-          description: "outro sem imagem",
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
           section: "",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-projeto-5",
-          title: 'projeto 5',
-          description: "um projeto com imagem de fundo",
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
           section: "",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-projeto-6",
-          title: 'projeto 6',
-          description: "um projeto sem imagem",
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
           section: "",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
